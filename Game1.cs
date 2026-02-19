8d91fa0ae7f01d00d685e4f9d1b8e92967d5644b
Annie Wang <anniewang@utexas.edu>
2026-02-19 17:52:46 -0600

    Add

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace group_19_assignment4;
 
@@ -8,9 +9,14 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-
+    
+    // Background
     private Texture2D _background;
     
+    // Music Variable
+    private Song _bgm;
+    
+    // Piranha Plant
     private PiranhaPlant piranha1;
     private PiranhaPlant piranha2;
 
@@ -23,24 +29,34 @@ public class Game1 : Game
     
     protected override void Initialize()
     {
+        // Background Size
         _graphics.PreferredBackBufferWidth = 778;
         _graphics.PreferredBackBufferHeight = 350;
         _graphics.ApplyChanges();
         
         base.Initialize();
     }
-
+    
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // Background Sound
+        _bgm = Content.Load<Song>("background_music");
+        MediaPlayer.Volume = 0.3f; 
+        MediaPlayer.IsRepeating = true; 
+        MediaPlayer.Play(_bgm);
+        
+        // Background
         _background = Content.Load<Texture2D>("background");
 
+        // Piranha Plant
         Texture2D stem = Content.Load<Texture2D>("Stem");
         Texture2D jLeft = Content.Load<Texture2D>("Jaw Left");
         Texture2D jRight = Content.Load<Texture2D>("Jaw Right");
         
-        piranha1 = new PiranhaPlant(stem, jLeft, jRight, new Vector2(610, 245), 3.0f); // left
-        piranha2 = new PiranhaPlant(stem, jLeft, jRight, new Vector2(687, 215), 6.0f); // right
+        piranha1 = new PiranhaPlant(stem, jLeft, jRight, new Vector2(610, 245), 3.0f); 
+        piranha2 = new PiranhaPlant(stem, jLeft, jRight, new Vector2(687, 215), 6.0f); 
     }
 
     protected override void Update(GameTime gameTime)
@@ -62,6 +78,7 @@ public class Game1 : Game
         _spriteBatch.Begin();
         _spriteBatch.Draw(_background, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
         _spriteBatch.End();
+        
         piranha1.Draw(_spriteBatch);
         piranha2.Draw(_spriteBatch);
         
