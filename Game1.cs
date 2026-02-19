aa87725ecada5f0e6d778ad28f55d98d2ff3d29c
Annie Wang <anniewang@utexas.edu>
2026-02-19 17:30:54 -0600

    Add

--- Game1.cs
+++ Game1.cs
@@ -10,6 +10,9 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
 
     private Texture2D _background;
+    
+    private PiranhaPlant piranha1;
+    private PiranhaPlant piranha2;
 
     public Game1()
     {
@@ -31,6 +34,13 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         _background = Content.Load<Texture2D>("background");
+
+        Texture2D stem = Content.Load<Texture2D>("Stem");
+        Texture2D jLeft = Content.Load<Texture2D>("Jaw Left");
+        Texture2D jRight = Content.Load<Texture2D>("Jaw Right");
+        
+        piranha1 = new PiranhaPlant(stem, jLeft, jRight, new Vector2(610, 245), 3.0f); // left
+        piranha2 = new PiranhaPlant(stem, jLeft, jRight, new Vector2(687, 215), 6.0f); // right
     }
 
     protected override void Update(GameTime gameTime)
@@ -38,6 +48,9 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
             Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+        
+        piranha1.Update(gameTime);
+        piranha2.Update(gameTime);
 
         base.Update(gameTime);
     }
@@ -45,11 +58,13 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
-        _spriteBatch.Begin();
         
+        _spriteBatch.Begin();
         _spriteBatch.Draw(_background, new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), Color.White);
         _spriteBatch.End();
-
+        piranha1.Draw(_spriteBatch);
+        piranha2.Draw(_spriteBatch);
+        
         base.Draw(gameTime);
     }
 }
--- PiranhaPlant.cs
+++ PiranhaPlant.cs
@@ -1,6 +1,65 @@
-﻿namespace group_19_assignment4;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 
-public class PiranhaPlant
+namespace group_19_assignment4
 {
-    
+    public class PiranhaPlant
+    {
+        private Texture2D _stemTex;
+        private Texture2D _jawRightTex;
+        private Texture2D _jawLeftTex;
+        
+        private Vector2 _basePosition;
+        private float _extensionY;
+        private float _jawRotation;
+        private float _snapSpeed;
+        private float _timer;
+        
+        private Matrix _rootMatrix;
+
+        public PiranhaPlant(Texture2D stem, Texture2D jawLeft, Texture2D jawRight, Vector2 basePos, float snapSpeed)
+        {
+            _stemTex = stem;
+            _jawLeftTex = jawLeft;
+            _jawRightTex = jawRight;
+            _basePosition = basePos;
+            _snapSpeed = snapSpeed;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            
+            _extensionY = MathF.Sin(_timer * 3) * 17f - 25f; 
+            
+            _rootMatrix = Matrix.CreateScale(0.15f) * Matrix.CreateTranslation(_basePosition.X, _basePosition.Y + _extensionY, 0);
+
+            _jawRotation = MathF.Abs(MathF.Sin(_timer * _snapSpeed)) * 0.6f;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin(transformMatrix: _rootMatrix);
+
+            spriteBatch.Draw(_stemTex, Vector2.Zero, Color.White);
+
+            float leftOffset = 8.5f;
+            float verticalPosition = 0f;
+                
+            Vector2 leftHinge = new Vector2(_jawLeftTex.Width, _jawLeftTex.Height); 
+            spriteBatch.Draw(_jawLeftTex, 
+                new Vector2((_stemTex.Width / 2) - leftOffset, verticalPosition),
+                null, Color.White, -_jawRotation, leftHinge, 1.0f, SpriteEffects.None, 0); // left
+            Vector2 rightHinge = new Vector2(0, _jawRightTex.Height);
+            spriteBatch.Draw(_jawRightTex, 
+                new Vector2((_stemTex.Width / 2) - leftOffset, verticalPosition),
+                null, Color.White, _jawRotation, rightHinge, 1.0f, SpriteEffects.None, 0); // right
+            spriteBatch.End();
+        }
+    }
 }
+
+
+
+
